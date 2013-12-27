7b62e78a23b21e762dc46f5475c627e413c0c40c
Marina Solovyova <marinka.solovyova@gmail.com>
2013-12-27 22:05:58 +0200

    Server 2 with log file

--- server2.py
+++ server2.py
@@ -5,9 +5,7 @@ socket = context.socket(zmq.SUB)
 socket.connect("epgm://192.168.1.18:5555")
 socket.setsockopt(zmq.SUBSCRIBE, "")
  
-print "Server1 is waiting"
+print "Server2 is waiting"
 while True:
-    with open('./server-log', 'a') as server_log:	    
-        message = socket.recv()
-        server_log.write(message)
-    
+    message = socket.recv()
+    print "Received request: ", message
